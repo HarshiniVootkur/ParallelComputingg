4c54f5abceaab541d45911452ef123cc3379ab81
student <hvootkur@uncc.edu>
2021-02-02 18:52:48 -0500

    activity hello programming done

--- activity_hello/hello.cpp
+++ activity_hello/hello.cpp
@@ -1,4 +1,13 @@
-int main () {
 
-  return 0;
+
+#include<iostream>
+#include<unistd.h>
+int main()
+{
+  char hostname[1024]="";
+  int val=gethostname(hostname,sizeof hostname);
+  if(val==-1)
+    std::cout<<"Error!!Please Check Again\n";
+  else
+    std::cout<<"Host Name is:\t"<<hostname;
 }
