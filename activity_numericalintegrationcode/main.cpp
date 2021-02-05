e5f485e2c3b73d80d0d0aa2cbea95f86447f5dda
Harshini Reddy Vootkur <hvootkur@mba-i1.uncc.edu>
2021-02-05 16:57:39 -0500

    Done changes to program

--- activity_numericalintegrationcode/main.cpp
+++ activity_numericalintegrationcode/main.cpp
@@ -16,41 +16,47 @@ float f4(float x, int intensity);
 }
 #endif
 
-  
-int main (int argc, char* argv[]) {
+int index,functionid,a,b,n,intensity;
+float integral,diff,function=0,c;
 
-  auto sample=std::chrono::system_clock::now();
-  
-  if (argc < 6) {
-    std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity>"<<std::endl;
-    return -1;
-  }
-  int index,functionid,a,b,n,intensity,c;
-  float integral;
-  functionid=strtof(argv[1],NULL);
-  a=strtof(argv[2],NULL);
-  b=strtof(argv[3],NULL);
-  n=strtof(argv[4],NULL);
-  intensity=strtof(argv[5],NULL);
-
-  c=(b-a)/n;
- 
-      for(index=0;index<=n-1;index++)
+int main (int argc, char* argv[]) 
+{
+	auto starttime=std::chrono::system_clock::now();  
+	if(argc < 6) 
 	{
-	 integral=(c*((a+(index+0.5)*c))*n);
+		std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity>"<<std::endl;
+		return -1;
 	}
-      std::cout<<"Integral=\t"<<integral<<"\n";
-      if(functionid==1)
-	f1(integral,intensity);
-      else if(functionid==2)
-	f2(integral,intensity);
-      else if(functionid==3)
-	f3(integral,intensity);
-      else if(functionid==4)
-	  f4(integral,intensity);
-      else
-	std::cout<<"ERROR!!\n";
-	     
-      
-  return 0;
+
+	functionid=strtof(argv[1],NULL);
+	a=strtof(argv[2],NULL);
+	b=strtof(argv[3],NULL);
+	n=strtof(argv[4],NULL);
+	intensity=strtof(argv[5],NULL);
+
+	diff=(b-a);
+	c=diff/n;
+  
+	for(index=0;index<=n-1;index++)
+	{
+		integral=(a+((index+0.5)*c));
+		if(functionid==1)
+			function=function+f1(integral,intensity);
+		else if(functionid==2)
+			function=function+f2(integral,intensity);
+		else if(functionid==3)
+			function=function+f3(integral,intensity);
+		else if(functionid==4)
+			function=function+f4(integral,intensity);
+		else
+			std::cout<<"ERROR!!\n";
+	}
+	function=function*c;
+
+	auto endtime=std::chrono::system_clock::now();
+	std::cout<<function<<"\n";
+	
+	std::chrono::duration<float> timetaken=endtime-starttime;
+	std::cerr<<timetaken.count()<<"\n";
+	return 0;
 }
